d5a84478ea21e1302807d9cdf227b672a2776b46
Kuxwpel <kuxwpel@gmail.com>
2022-04-23 01:08:09 +0200

    code

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Home from "./components/Home";
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About /> 
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
